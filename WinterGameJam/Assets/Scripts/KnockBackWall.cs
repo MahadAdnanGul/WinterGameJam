c9361c02afbeb320f3c96371dd7b0758d56c72b4
Muhammad Junaid <21100025@lums.edu.pk>
2021-01-25 03:26:06 +0500

    Revert "Revert "Added Obstacles and knockback feature""
    This reverts commit b397b5042706a98dec1a9c8ced72a14d078d448c.

---
+++ WinterGameJam/Assets/Scripts/KnockBackWall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class KnockBackWall : MonoBehaviour
+{
+    [SerializeField] float knockForce = 500f;
+    [SerializeField] float knockDuration = 0.5f;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        PlayerController controller = other.gameObject.GetComponent<PlayerController>();
+        if(controller != null)
+        {
+            // rb.AddRelativeForce(knockForce * -Vector3.forward);
+            controller.Disable(knockForce, knockDuration);
+        }
+    }
+}
