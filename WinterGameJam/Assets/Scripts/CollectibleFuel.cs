8527895328d5a745faa6c7c6829ecf20ee977ce6
Muhammad Junaid <21100025@lums.edu.pk>
2021-01-25 01:35:38 +0500

    Map and Fuel Collectibles
    Added a basic map and fuel drops + can collectibles.
    
    Changed lighting settings a little bit

---
+++ WinterGameJam/Assets/Scripts/CollectibleFuel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CollectibleFuel : MonoBehaviour
+{
+    [SerializeField] float refillAmount = 2f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController controller = other.gameObject.GetComponent<PlayerController>();
+        if(controller != null)
+        {
+            controller.fuelAmount += controller.fuelCapacity > controller.fuelAmount ? refillAmount : 0f;
+        }
+        Destroy(gameObject);
+    }
+}
