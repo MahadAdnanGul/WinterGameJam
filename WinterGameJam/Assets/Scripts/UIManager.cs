30d859dbb1f7cad81b94999cbc99d34a4dd894f6
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2021-01-31 04:07:31 +0500

    Game overhaul, mobile controls, level by level
    lots of stuff

---
+++ WinterGameJam/Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    public void PauseTime()
+    {
+        Time.timeScale = 0;
+    }
+    public void ResumeTime()
+    {
+        Time.timeScale = 1;
+    }
+}
